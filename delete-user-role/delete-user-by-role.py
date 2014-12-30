dbfdac88d678e172e5d4b25f2214bde7624e7b6b
Kelly Gerrow <kgerrow@esri.com>
2014-12-30 14:28:56 -0800

    adding comments

--- delete-user-role/delete-user-by-role.py
+++ delete-user-role/delete-user-by-role.py
@@ -19,77 +19,45 @@ import re ## DBUG
 #Warning, This script is designed to remove all content regardless of delete
 #protection. Please use carefully or remove the protected content sections
 
-def GetCustomRoles(token):
-
-    RolesURL = 'http://{}.maps.arcgis.com/sharing/rest/portals/self/roles?f=json&token={}'.format(URLKey, token)
-    response = requests.get(RolesURL)
-    Roles = json.loads(response.text)['roles']
-    CustomRolesDict = {}
-    for role in Roles:
-        CustomRolesDict[role['name']] = role['id']
-
-    return CustomRolesDict
-
+#creates a list of users with the role to be deleted
 def listUserRole(userDict, role):
     userlist=[]
     for item in userDict:
             if item['role'] == role:
                 userlist.append(item['username'])
 
-
-##        # ========================== DREW TESTING ==============================
-##        ''' Catch and report any invalid characters in the username.
-##            Issue is most likely encoding. Change the encoding with the
-##            'encoding' property of the response object.
-##            (example: response.encoding = 'ISO-8859-1') -- Drew'''
-##        response = requests.get(request) # default encoding is UTF-8
-##        output = response.text
-##        try:
-##            jres = json.loads(output)
-##        except ValueError as error_message:
-##            char_position = re.findall(r'char (\d*)', str(error_message))[0]
-##            invalid_char = output[int(char_position):int(char_position) + 1]
-##            error_data = (response.encoding, invalid_char, char_position)
-##            print "ValueError found:",
-##            print "Encoding {0} found character {1} at position {2}".format(*error_data)
-##        # ======================================================================
-
     return userlist
-
-
-
 
 if __name__ == '__main__':
 
-    #variables
+    #can hardcode username, password and role if scheduling as a task
+    #Input Variables
     user = raw_input("Admin username:")
     pw  = raw_input("Password:")
-
     role = raw_input("What role do you want to delete?")
 
+    #Calls AGOL admin class
     t = excalibur.agolAdmin(user, pw)
 
+    #exchanges custom Role Name for Role ID
     SelectedRole = t.roleAssign(role)
 
-    #get list of users to be removed
+    #get list of users to be removed based on Role
     userlist = listUserRole(t.userDict, SelectedRole)
 
-    sort_list = sorted(userlist)
-    for x in sort_list:
-        print x
-    print "\nTOTAL USERS: " + str(len(userlist))
+    print "\nTOTAL USERS TO BE DELETED: " + str(len(userlist))
     print userlist
-     ## DBUG
-##    raise SystemExit # NO, PYTHON, STOP!!!
+
     for item in userlist:
 
-        #delete user content:
+        #delete all user content:
         delCont = t.delUserContent(item)
 
-        #delete user groups
+        #delete user owned groups
         delGroup = t.delUserGroups(item)
 
-        #delete user
-        delUsers = t.delUser(item)
+        #delete user from AGOL (will be able to reuse name)
+        #uncomment if you want to actually remove username
+        #delUsers = t.delUser(item)
 
 
